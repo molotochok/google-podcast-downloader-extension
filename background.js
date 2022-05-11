8c26f53a441f3228e09b8440681160980979e120
Markiian Karpa <markian98@gmail.com>
2022-05-11 22:55:25 +0200

    Reload pages when enabling, installing, etc

--- background.js
+++ background.js
@@ -1,10 +1,15 @@
-chrome.runtime.onInstalled.addListener(() => {
+function reloadPages() {
   chrome.tabs.query({url: "*://podcasts.google.com/*"}, (tabs) => {
     tabs.forEach((tab) => {
       chrome.tabs.reload(tab.id);
     });
   });
-});
+}
+
+chrome.management.onEnabled.addListener(reloadPages);
+chrome.management.onInstalled.addListener(reloadPages);
+chrome.management.onDisabled.addListener(reloadPages);
+chrome.management.onUninstalled.addListener(reloadPages);
 
 chrome.runtime.onMessage.addListener((arg, _sender, _sendResponse) => {
   chrome.downloads.download(
